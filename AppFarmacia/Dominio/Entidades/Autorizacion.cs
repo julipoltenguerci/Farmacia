4454b12ef04befc522dfed92a99e086d03b87e80
julipoltenguerci <jpoltenguerci@gmail.com>
2021-10-26 14:00:01 -0300

    Se añaden clases faltantes del dominio

---
+++ AppFarmacia/Dominio/Entidades/Autorizacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Autorizacion
+    {
+        public int IdAutorizacion { get; set; }
+        public bool Estado { get; set; }
+        public DateTime Fecha { get; set; }
+        public Medico Medico { get; set; }
+        public ObraSocial ObraSocial { get; set; }
+
+        public Autorizacion(int idAutorizacion, bool estado, DateTime fecha, Medico medico, ObraSocial obraSocial)
+        {
+            IdAutorizacion = idAutorizacion;
+            Estado = estado;
+            Fecha = fecha;
+            Medico = medico;
+            ObraSocial = obraSocial;
+        }
+    }
+}
---
+++ AppFarmacia/Dominio/Entidades/TipoSuministro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class TipoSuministro
+    {
+        public int IdTipoSuministro { get; set; }
+        public string NomTipoSuministro { get; set; }
+
+        public TipoSuministro(int idTipoSuministro, string nomTipoSuministro)
+        {
+            IdTipoSuministro = idTipoSuministro;
+            NomTipoSuministro = nomTipoSuministro;
+        }
+    }
+}
